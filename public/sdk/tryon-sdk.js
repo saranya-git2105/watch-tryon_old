d8743508868a3eb37c4174c910477e7bb1e38888
Chnagasaranya <nagasaranya@gmail.com>
2026-05-19 17:34:31 +0530

    change VTO_HOST

--- public/sdk/tryon-sdk.js
+++ public/sdk/tryon-sdk.js
@@ -8,7 +8,7 @@
 
   // Server URL of the hosted Watch Try-On system
   // Configured to point directly to your active public dev tunnel for live mobile sync
-  const VTO_HOST = 'https://0cj9g3c7-3001.inc1.devtunnels.ms';
+  const VTO_HOST = 'http://20.197.47.191:3221/sdk/tryon-sdk.js';
 
   // Global styles for the premium CTA button and modal overlay
   const STYLES = `
